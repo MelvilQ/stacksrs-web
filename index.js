b8df4c7de764c8b6bc024ddb4a5cebbbd1028698
melvil <paradise-interactive@web.de>
2018-12-16 16:01:38 +0100

    reverse deck

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-if('serviceWorker' in navigator){
+if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js');
 }
 
 const storage = window.localStorage;
 
-function isLocalStorageSupported(){
+function isLocalStorageSupported() {
   try {
     const key = "__some_random_key_you_are_not_going_to_use__123";
     storage.setItem(key, key);
@@ -38,41 +38,41 @@ var app = new Vue({
     filter: ''
   },
   computed: {
-    filteredCards: function(){
+    filteredCards: function () {
       return this.currentDeck.cards.filter(card => (card.front + "->" + card.back).includes(this.filter));
     }
   },
   methods: {
-    showError: function(msg){
+    showError: function (msg) {
       alert(msg);
     },
-    loadLocalDeckList: function(){
+    loadLocalDeckList: function () {
       let json = storage.getItem('decklist');
-      if(!json){
+      if (!json) {
         this.deckList = [];
       } else {
         try {
           this.deckList = JSON.parse(json);
-        } catch(e){
+        } catch (e) {
           console.log(e);
           this.showError('Could not load local deck list.');
           this.deckList = [];
         }
       }
     },
-    saveLocalDeckList: function(){
-      if(!this.deckList){
+    saveLocalDeckList: function () {
+      if (!this.deckList) {
         this.showError('Cannot save empty deck list.');
         return;
       }
       try {
         storage.setItem('decklist', JSON.stringify(this.deckList));
-      } catch(e){
+      } catch (e) {
         console.log(e);
         this.showError('Could not save local deck list.');
       }
     },
-    updateLocalDeckList: function(){
+    updateLocalDeckList: function () {
       this.deckList.filter(deck => deck.file === this.currentDeck.file).forEach(deck => {
         deck.total = this.currentDeck.cards.length;
         deck.known = this.currentDeck.cards.filter(card => card.level > 2).length;
@@ -80,25 +80,25 @@ var app = new Vue({
       });
       this.saveLocalDeckList();
     },
-    loadLocalDeck: function(file){
+    loadLocalDeck: function (file) {
       try {
         this.currentDeck = JSON.parse(storage.getItem('deck_' + file));
         this.goToReview();
-      } catch(e){
+      } catch (e) {
         console.log(e);
         this.showError('Could not load deck.');
       }
     },
-    saveCurrentDeck: function(){
+    saveCurrentDeck: function () {
       try {
         storage.setItem('deck_' + this.currentDeck.file, JSON.stringify(this.currentDeck));
         this.updateLocalDeckList();
-      } catch(e){
+      } catch (e) {
         console.log(e);
         this.showError('Could not save current deck.');
       }
     },
-    loadServerDeckList: function(){
+    loadServerDeckList: function () {
       axios.get('decks.txt').then(response => {
         this.onlineDeckList = response.data.decks;
       }).catch(e => {
@@ -106,8 +106,8 @@ var app = new Vue({
         this.showError('Could not load online deck collection. Please try it again later.');
       });
     },
-    importServerDeck: function(file){
-      if(this.deckList.filter(deck => deck.file === file).length){
+    importServerDeck: function (file) {
+      if (this.deckList.filter(deck => deck.file === file).length) {
         this.loadLocalDeck(file);
         return;
       }
@@ -127,73 +127,73 @@ var app = new Vue({
         this.showError('Could not import deck. Please try it again later.');
       });
     },
-    importFromFile(file){
+    importFromFile(file) {
       // TODO
     },
-    createNewDeck(){
+    createNewDeck() {
       // TODO
     },
-    saveCurrentDeckToFile(){
+    saveCurrentDeckToFile() {
       download(this.currentDeck.file + '.txt', JSON.stringify(this.currentDeck));
     },
-    deleteLocalDeck(file){
+    deleteLocalDeck(file) {
       storage.removeItem('deck_' + file);
       this.deckList = this.deckList.filter(deck => deck.file !== file);
       this.saveLocalDeckList();
-      if(!this.deckList.length){
+      if (!this.deckList.length) {
         this.goToServerDecks();
       }
     },
-    tryLoadingDeck(file){
+    tryLoadingDeck(file) {
       let key = 'deck_' + file;
-      if(key in storage){
+      if (key in storage) {
         this.loadLocalDeck(file);
       } else {
         this.importServerDeck(file);
       }
     },
-    goToReview: function(){
+    goToReview: function () {
       console.log('switch to review');
       history.pushState(this.currentDeck.file, document.title, '#' + this.currentDeck.file);
       this.page = 'review';
       this.showNextCard();
     },
-    goToLocalDecks: function(){
+    goToLocalDecks: function () {
       console.log('switch to local deck list');
       history.pushState('', document.title, '#');
       this.page = 'local';
     },
-    goToServerDecks: function(){
+    goToServerDecks: function () {
       console.log('switch to server deck list');
       history.pushState('', document.title, '#');
       this.page = 'online';
     },
-    goToDeckBrowser: function(){
+    goToDeckBrowser: function () {
       console.log('switch to deck browser');
       this.page = 'browser';
     },
-    goToAboutPage: function(){
+    goToAboutPage: function () {
       console.log('switch to about page');
       history.pushState('about', document.title, '#about');
       this.page = 'about';
     },
-    showNextCard: function(){
+    showNextCard: function () {
       this.isShowingAnswer = false;
       this.front = this.currentDeck.cards[0].front;
       this.back = this.currentDeck.cards[0].back;
     },
-    continue: function(){
-      if(this.isShowingAnswer){
+    continue: function () {
+      if (this.isShowingAnswer) {
         this.correct();
       } else {
         this.showAnswer();
       }
     },
-    showAnswer: function(){
+    showAnswer: function () {
       this.isShowingAnswer = true;
     },
-    wrong: function(){
-      if(this.page !== 'review' || !this.isShowingAnswer){
+    wrong: function () {
+      if (this.page !== 'review' || !this.isShowingAnswer) {
         return;
       }
       let card = this.currentDeck.cards.shift();
@@ -203,14 +203,14 @@ var app = new Vue({
       this.saveCurrentDeck();
       this.showNextCard();
     },
-    correct: function(){
-      if(this.page !== 'review' || !this.isShowingAnswer){
+    correct: function () {
+      if (this.page !== 'review' || !this.isShowingAnswer) {
         return;
       }
       let card = this.currentDeck.cards.shift();
       card.level += 1;
       let position = 99999999;
-      if(card.level < 10){
+      if (card.level < 10) {
         // this is the magic SRS formula:
         // level 1 => 16 cards later
         // level 2 => 64 cards later
@@ -225,44 +225,49 @@ var app = new Vue({
       this.saveCurrentDeck();
       this.showNextCard();
     },
-    shuffleCurrentDeck: function(){
+    shuffleCurrentDeck: function () {
       this.currentDeck.cards = _.shuffle(this.currentDeck.cards);
       this.saveCurrentDeck();
       this.showNextCard();
     },
-    resetStrengthOfCurrentDeck(level){
+    reverseCurrentDeck: function () {
+      this.currentDeck.cards = this.currentDeck.cards.reverse();
+      this.saveCurrentDeck();
+      this.showNextCard();
+    },
+    resetStrengthOfCurrentDeck(level) {
       this.currentDeck.cards.forEach(card => {
         card.level = level;
       });
       this.saveCurrentDeck();
     },
-    flagUrl: function(lang){
+    flagUrl: function (lang) {
       return lang + '.svg';
     },
-    highlightColor: function(card){
-      if(card.level > 2){
+    highlightColor: function (card) {
+      if (card.level > 2) {
         return 'well-known';
-      } else if(card.level === 0){
+      } else if (card.level === 0) {
         return 'new';
       } else {
         return 'learning';
       }
     }
   },
-  mounted: function(){
-    if(!isLocalStorageSupported()){
+  mounted: function () {
+    if (!isLocalStorageSupported()) {
       this.showError('Your browser does not support local storage. Please use Chrome or Firefox.');
       return;
     }
-    if('decklist' in storage){
+    if ('decklist' in storage) {
       this.loadLocalDeckList();
     }
     let startDeck = window.location.hash ? window.location.hash.replace(/#/g, '') : null;
-    if(startDeck === 'about'){
+    if (startDeck === 'about') {
       this.goToAboutPage();
-    } else if(startDeck){
+    } else if (startDeck) {
       this.tryLoadingDeck(startDeck);
-    } else if(this.deckList && this.deckList.length){
+    } else if (this.deckList && this.deckList.length) {
       this.goToLocalDecks();
     } else {
       this.goToServerDecks();
